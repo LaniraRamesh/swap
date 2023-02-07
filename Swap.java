94e9cdd006e115372c059d054e06b090a0995d1f
LaniraRamesh <123591205+LaniraRamesh@users.noreply.github.com>
2023-02-07 09:35:20 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,18 @@
+package trial01;
+
+public class Swap {
+	//with temp variables//
+	public static void main(String[] args) {
+		
+	
+	int a=10;
+	int b=20;
+	int temp;
+	temp=a;
+	a=b;
+	b=temp;
+	System.out.println("a is "+a);
+	System.out.println("b is "+b);
+	
+	}
+}
